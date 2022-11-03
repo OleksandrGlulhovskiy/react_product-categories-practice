43f0aa54383fa619bda548040cc499cc09e92802
Oleksandr <sanino343434@gmail.com>
2022-11-03 11:26:59 +0200

    Practice solution

---
+++ src/components/Products.tsx
@@ -0,0 +1,31 @@
+import { ProductWithCategory } from '../react-app-env';
+import { ProductCategory } from './ProductCategory';
+import { ProductUser } from './ProductUser';
+
+type Props = {
+  product: ProductWithCategory,
+};
+
+export const Products: React.FC<Props> = ({ product }) => {
+  const {
+    id,
+    name,
+    category,
+    user,
+  } = product;
+
+  return (
+    <tr data-cy="Product">
+      <td className="has-text-weight-bold" data-cy="ProductId">
+        {id}
+      </td>
+
+      <td data-cy="ProductName">{name}</td>
+      {category
+        ? <ProductCategory category={category} />
+        : 'No category'}
+
+      <ProductUser user={user} />
+    </tr>
+  );
+};
